b8aee2770a3080ae6df9b937211df6ffd2b294ca
Dethcrvsh <fabianfabbe@hotmail.se>
2024-12-08 13:46:03 +0100

    day 5

--- day-5.py
+++ day-5.py
@@ -1,15 +1,8 @@
 from aoc import get_puzzle
-test = "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47"
+from functools import cmp_to_key
 
 
-def is_right(pages, rules):
-    for check, ps in zip(pages, [pages[i+1:] for i in range(len(pages))]):
-        if any(rules[p] and check in rules[p] for p in ps):
-            return False
-    return True
-
-
-def part1():
+def parse_input():
     inp = get_puzzle(5).split("\n\n")
     inp_rules = inp[0].splitlines()
     pages = [list(map(int, r.split(","))) for r in inp[1].splitlines()]
@@ -19,11 +12,31 @@ def part1():
         i = r.split("|")
         rules[int(i[0])] += [int(i[1])]
 
-    return sum(r[len(r)//2] for r in filter(lambda p: is_right(p, rules), pages))
+    return pages, rules
+
+
+def is_right(pages, rules):
+    for check, ps in zip(pages, [pages[i + 1 :] for i in range(len(pages))]):
+        if any(rules[p] and check in rules[p] for p in ps):
+            return False
+    return True
+
+
+def part1():
+    pages, rules = parse_input()
+    return sum(r[len(r) // 2] for r in filter(lambda p: is_right(p, rules), pages))
 
 
 def part2():
-    pass
+    pages, rules = parse_input()
+
+    def compare(p1, p2):
+        return -1 if p1 not in rules[p2] else 0
+
+    pages_updated = map(lambda p: sorted(p, key=cmp_to_key(compare)), pages)
+    pages_correct = filter(lambda p: p not in pages, pages_updated)
+    
+    return sum(r[len(r) // 2] for r in pages_correct)
 
 
 if __name__ == "__main__":
